54526a7128bf6e20125e83047b8ab5be024dbce9
Andrew <figol_st@ukr.net>
2018-07-31 18:19:08 +0000

    js class 30/07

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//let hello = 'Helo world';
+//alert('hello');
+
+/*const userName = prompt('name');
+const gritings = 'Привіт '+ userName;
+alert(gritings);
+
+const userAge = parseInt(prompt('Enter your age'));
+alert('Твій вік ' + userAge)
+
+const nextAge = userAge + 1;
+alert('Наступного року тобі буде ' + nextAge);
+
+if (userAge >= 18) {
+    alert('You are adult');
+} else {
+    alert('You are child');
+}
+*/
+function add (a, b, c) {
+    const result = a+b+c;
+    return result;
+}
+const res = add(1,2,3);
+alert(res);
